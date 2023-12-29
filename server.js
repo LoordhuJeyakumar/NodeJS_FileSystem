59722deffe7e5107cadbd01c0665cc03d5342d84
LoordhuJeyakumar <loordhujeyakumar@gmail.com>
2023-12-29 06:31:46 +0530

    createFile and files end point created

---
+++ server.js
@@ -0,0 +1,56 @@
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+const { error } = require("console");
+const app = express();
+
+const PORT = 3001;
+
+app.get("/", (request, response) => {
+  response.send("<h1>NodeJS FileSystem</h1>");
+});
+
+app.post("/api/createFile", (request, response) => {
+  createFile(request, response);
+});
+
+app.get("/api/files", (request, response) => {
+  retriveFiles(request, response);
+});
+
+function createFile(request, response) {
+  const timestamp = Date.now();
+
+  const date = new Date(timestamp).toDateString();
+  const time = new Date(timestamp).toLocaleTimeString();
+
+  let fileName = `${date} - ${time.split(":").join("_")}.txt`;
+
+  fs.writeFile(
+    path.join(__dirname, "files", fileName),
+    `${timestamp}`,
+    (error) => {
+      if (error) throw error;
+      response.status(201).json({ message: "File created successfully" });
+      console.log("File Created Succesfully");
+    }
+  );
+}
+
+function retriveFiles(request, response) {
+  try {
+    const files = fs.readdirSync(path.resolve("./files"));
+    if (files.length !== 0) {
+      response.status(200).json({ files });
+      console.log(files);
+    } else {
+      response.status(200).json({ message: "there is no files " });
+    }
+  } catch (error) {
+    response.status(404).json({ message: "there is no such directory" });
+  }
+}
+
+app.listen(PORT, () => {
+  console.log(`server running on ${PORT}`);
+});
